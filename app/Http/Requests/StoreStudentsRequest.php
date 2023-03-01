165f99f4b7908097c42b7d9ada1e51bfdcbea347
Ronal62 <ichojuraku@gmail.com>
2023-03-01 08:32:50 +0700

    Laravel_Pengaduan

---
+++ app/Http/Requests/StoreStudentsRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreStudentsRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
+     */
+    public function rules(): array
+    {
+         return [
+        'txtid' => 'required|unique:students,id_students|min:7|max:7',
+        'txtfull_name' => 'required',
+        'txtgender' => 'required',
+        'txtemail' => 'required',
+        'txtphone' => 'required',
+        'txtaddress' => 'required',
+    ];
+    }
+}
